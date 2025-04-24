6c4d521f22fb725609ec64872d9d937243904787
Worldbuilding-god <cte-ronald.smith@sllboces.org>
2025-04-24 00:24:39 +0000

    opioukyjuiuo

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-$(."button").click(function(){
-    if($(".button").text == "Protanopia"){
+$(".button").click(function(){
+    if($(this).text == "Protanopia"){
         $(".lgreen").css("background-color", "rgb(227, 227, 216)");
     }
-})
+});
