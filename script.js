b51f5160fcf56db00776a28c14d207faa12e2ae6
Worldbuilding-god <cte-ronald.smith@sllboces.org>
2025-04-29 17:45:56 +0000

    tymgnf

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-$(".button").click(function(){
-    if($(this).text == "Protanopia"){
-        $(".lgreen").css("background-color", "rgb(227, 227, 216)");
-    }
-});
+//var cb1 = document.getElementById("protanopia");
+var lgreen = document.getElementsByClassName(lgreen);
+var mgreen = document.getElementsByClassName(mgreen);
+var dgreen = document.getElementsByClassName(dgreen);
+//cb1.addEventListener('click', function() {
+    //lgreen.style.backgroundColor = "yellow";
+    //$(".lgreen").css("background-color", "rgb(227, 227, 216)");});
+function protanopiaColorChange() {
+    lgreen.style.backgroundColor = "yellow";
+    $(".lgreen").css("background-color", "rgb(227, 227, 216)");
+}
+//$(".button").click(function(){
+    //if($(this).text == "Protanopia"){
+       //$(".lgreen").css("background-color", "rgb(227, 227, 216)");};});
