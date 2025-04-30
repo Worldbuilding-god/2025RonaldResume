c7222e6db66e76b8610fba79bfc351a90bf33b79
Worldbuilding-god <cte-ronald.smith@sllboces.org>
2025-04-30 17:37:54 +0000

    nbgvgnb

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-//var cb1 = document.getElementById("protanopia");
-var lgreen = document.getElementsByClassName(lgreen);
-var mgreen = document.getElementsByClassName(mgreen);
-var dgreen = document.getElementsByClassName(dgreen);
-//cb1.addEventListener('click', function() {
-    //lgreen.style.backgroundColor = "yellow";
-    //$(".lgreen").css("background-color", "rgb(227, 227, 216)");});
 function protanopiaColorChange() {
-    lgreen.style.backgroundColor = "yellow";
-    $(".lgreen").css("background-color", "rgb(227, 227, 216)");
-}
-//$(".button").click(function(){
-    //if($(this).text == "Protanopia"){
-       //$(".lgreen").css("background-color", "rgb(227, 227, 216)");};});
+    //$("html").css("background-color", "rgb(179, 179, 175)");
+    //$(".boxkind1").css("background-color", "rgb(227, 227, 216)");
+    //$("button").css("background-color", "rgb(179, 179, 175)");
+    //$("nav").css("background-color", "rgb(227, 227, 216)");
+    $("html").toggleClass("dpro");
+    $(".boxkind1").toggleClass("lpro");
+    $("button").toggleClass("dpro");
+    $("nav").toggleClass("lpro");
+};
+function deuteranopiaColorChange() {
+    //$("html").css("background-color", "rgb(178, 176, 176)");
+    //$(".boxkind1").css("background-color", "rgb(226, 225, 217)");
+    //$("button").css("background-color", "rgb(178, 176, 176)");
+    //$("nav").css("background-color", "rgb(226, 225, 217)");
+    $("html").toggleClass("ddue");
+    $(".boxkind1").toggleClass("ldue");
+    $("button").toggleClass("ddue");
+    $("nav").toggleClass("ldue");
+};
+function tritanopiaColorChange() {
+    //$("html").css("background-color", "rgb(171, 179, 180)");
+    //$(".boxkind1").css("background-color", "rgb(220, 221, 222)");
+    //$("button").css("background-color", "rgb(171, 179, 180)");
+    //$("nav").css("background-color", "rgb(220, 221, 222)");
+    $("html").toggleClass("dtri");
+    $(".boxkind1").toggleClass("ltri");
+    $("button").toggleClass("dtri");
+    $("nav").toggleClass("ltri");
+};
+function dyslexiaButton(){
+    //$("p").css("font-size", "25px");
+    //$("button").css("font-size", "25px");
+    $("button").toggleClass("bigtext");
+    $("p").toggleClass("bigtext");
+};
+function darkButton(){
+    //$("html").css("background-color", "rgb(220, 238, 209)");
+    //$(".boxkind1").css("background-color", "rgb(170, 192, 170)");
+    //$("button").css("background-color", "rgb(220, 238, 209)");
+    //$("nav").css("background-color", "rgb(170, 192, 170)");
+    $("html").toggleClass("ldark");
+    $(".boxkind1").toggleClass("ddark");
+    $("button").toggleClass("ldark");
+    $("nav").toggleClass("ddark");
+};
+//function noneButton(){
+    //$("html").css("background-color", "rgb(170, 192, 170)");
+    //$("p").css("font-size", "16px");
+    //$("button").css("font-size", "13.3px");
+    //$(".boxkind1").css("background-color", "rgb(220, 238, 209)");
+    //$("button").css("background-color", "rgb(170, 192, 170)");
+    //$("nav").css("background-color", "rgb(220, 238, 209)");};
